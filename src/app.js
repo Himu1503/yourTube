bc6b8bac47c50c00a81355beb68f40b530f66af4
Himanshu Saxena <himanshu.saxena.india@gmail.com>
2024-01-22 17:29:20 +0000

    add register controller

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
-const app = express()
-
-
+import {ApiError} from "./utils/ApiError.js"
+const app =express()
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
@@ -22,5 +21,13 @@ app.use(express.static("public"))
 
 app.use(cookieParser())
 
+
+//Routes import 
+
+import userRouter from './routes/user.routes.js'
+
+//routes declaration 
+
+app.use('/api/v1/users/', userRouter)
 
 export {app}
